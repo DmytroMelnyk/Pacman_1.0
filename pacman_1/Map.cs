bd882e810e9bd64b5da67d90905dd58f56e74952
ValeriiMal <the.maltsews@gmail.com>
2015-01-14 16:47:47 +0200

    Commit solution and config files

---
+++ pacman_1/Map.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace pacman_1
+{
+    class Map
+    {
+        //fields
+
+        XDocument doc;
+        List<string> map;
+        int rows, columns;
+        
+        //constructors
+
+        public Map(string path)
+        {
+            doc = XDocument.Load(path);
+            CreateMap(doc);
+            rows = this.map.Count;
+            columns = this.map[0].Length;
+        }
+
+        // properties
+
+        public int Rows
+        {
+            get
+            {
+                return rows;
+            }
+        }
+
+        public int Columns 
+        {
+            get
+            {
+                return columns;
+            }
+        }
+
+        //methods
+
+        void CreateMap(XDocument xDoc)
+        {
+            map = new List<string>();
+            foreach (XElement el in xDoc.Elements("main").Elements("r"))
+            {
+                map.Add((string)el);
+            }
+        }
+
+        public void ShowMap()
+        {
+            Console.Clear();
+            int i;
+            for (i = 0; i < Rows - 1; i++)
+            {
+                Console.WriteLine(map[i]);
+            }
+            Console.Write(map[i]);
+        }
+
+    }
+}
---
+++ pacman_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace pacman_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create map
+            
+            Map map = new Map("H:/VS2012Proj/PACMAN/pacman_1/maps/map_2.xml");
+            SetConsoleWindow(map);
+            
+            
+
+            //start game
+            
+            //game
+
+            while (true)
+            {
+                if (!Console.KeyAvailable)
+                {
+
+                    Thread.Sleep(200);
+                    map.ShowMap();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            
+
+            Console.ReadLine();
+        }
+
+        static void SetConsoleWindow(Map map)
+        {
+            Console.SetBufferSize(100, map.Rows);
+            Console.SetWindowPosition(0, 0);
+            Console.SetWindowSize(map.Columns, map.Rows);
+        }
+        
+    }
+}
